7a4aaf7f10fb6c6b4f21c57819f47c31569dda0f
luizfsborges <luizfernandodasilvaborges@hotmail.com>
2023-12-04 15:16:56 -0300

    interface web e readme

--- ponderada_5/README.md
+++ ponderada_5/README.md
@@ -1,5 +1,70 @@
-# Aprimoramento do chatbot de navegação utilizando LLM e RAG
+# Chatbot de Especialista em Segurança com Retrieval Augmented Generation
 
-## 1. Enunciado
+Este guia fornece instruções passo a passo para configurar o Chatbot de Especialista em Segurança utilizando o Ollama e técnicas de RAG (Retrieval Augmented Generation) para contextualização.
 
-Utilizando um LLM (local ou API externa), crie um chatbot simples com instruções customizadas para ajudar um usuário a pesquisar normas de segurança em ambientes industriais. O sisema deve contar com uma interface gráfica e responder de forma sucinta e clara sobre o que lhe foi perguntado. 
+## Passos de Instalação
+
+### 1. Instale o Ollama
+
+```bash
+# Adicione o repositório do Ollama
+sudo add-apt-repository ppa:langchain/ollama
+sudo apt-get update
+
+# Instale o Ollama
+sudo apt-get install ollama
+```
+
+### 2. Instale o Gradio
+
+``` bash
+# Instale as dependências do Gradio
+sudo apt-get install python3-pip
+pip3 install gradio 
+```
+
+### 3. Configuração do ModelFile
+Crie um arquivo ModelFile chamado safety_expert_model.yml com o seguinte conteúdo:
+
+```yaml
+yaml
+Copy code
+FROM dolphin2.2-mistral
+
+PARAMETER temperature 1
+
+SYSTEM """
+From now on, you are designated as an expert on safety standards in industrial environments. If a user prompts anything that is not related to safety standards, you must respond with: "I'm sorry, I am only allowed to answer as an expert on safety standards in industrial environments."
+"""
+```
+
+### 4. Crie o Modelo de Especialista em Segurança
+
+Execute o seguinte comando para criar o modelo de Especialista em Segurança usando o Ollama:
+
+``` bash 
+Copy code
+ollama create safety_expert -f safety_expert_model.yml
+```
+
+### 5. Executando o Chatbot
+
+Clone o repositório contendo o script chatbot_seguranca_trabalho.py e navegue até o diretório.
+
+``` bash
+git clone <url_do_repositorio>
+
+cd <diretorio_do_repositorio>
+Execute o script do Chatbot de Especialista em Segurança:
+```
+
+```bash
+python3 chatbot_seguranca_trabalho.py
+```
+
+Acesse http://localhost:7860 em seu navegador para interagir com o Chatbot de Especialista em Segurança.
+
+### Instruções de uso
+- Digite suas perguntas relacionadas a normas de segurança em ambientes industriais na caixa de chat.
+
+- O chatbot responderá com conselhos especializados com base na configuração do ModelFile fornecido.
--- ponderada_5/chatbot_seguranca_trabalho_leitura.py
+++ ponderada_5/chatbot_seguranca_trabalho_leitura.py
@@ -1,4 +1,4 @@
-
+import gradio as gr
 from langchain.llms import Ollama
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnableMap, RunnablePassthrough
@@ -7,31 +7,55 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
-loader = TextLoader("./regras_workshop.txt")
-documents = loader.load()
+def setup_langchain():
+    loader = TextLoader("./regras_workshop.txt")
+    documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
 
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-vectorstore = Chroma.from_documents(docs, embedding_function)
-retriever = vectorstore.as_retriever()
+    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore = Chroma.from_documents(docs, embedding_function)
+    retriever = vectorstore.as_retriever()
 
-prompt = ChatPromptTemplate.from_template("""
-Answer the question based only on the following context:
-{context}
+    prompt = ChatPromptTemplate.from_template("""
+    Answer the question based only on the following context:
+    {context}
 
-Question: {question}
+    Question: {question}
 
-""")
+    """)
 
-model = Ollama(model="dolphin2.2-mistral")
+    model = Ollama(model="dolphin2.2-mistral")
 
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | model
-)
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | model
+    )
 
-for s in chain.stream("Which machinery do students can't opperate?"):
-    print(s, end="", flush=True)
+    return chain
+
+def main():
+    langchain_chain = setup_langchain()
+
+    def generate_response(text):
+        response = ""
+        for s in langchain_chain.stream(text):
+            response += s
+            iface.update("output", response)
+        return response
+
+    iface = gr.Interface(
+        fn=generate_response,
+        inputs=["text"],
+        outputs="text",
+        live=True,
+        title="LangChain Chat",
+        description="Answer questions based on context using LangChain",
+    )
+
+    iface.launch()
+
+if __name__ == "__main__":
+    main()
